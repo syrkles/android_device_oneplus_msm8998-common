fcfc62849bda184da4820c5d130e7a7171f3689f
liquid0624 <rjbirnesser@gmail.com>
2017-12-22 22:02:34 -0500

    msm8998-common: do not use legacy camera
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- common.mk
+++ common.mk
@@ -135,14 +135,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@3.2-impl \
+    libcamera_shim
+
+PRODUCT_PACKAGES += camera.device@3.2-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0
-
-PRODUCT_PACKAGES += \
-    libcamera_shim \
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
